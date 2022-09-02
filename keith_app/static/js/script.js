4f18c7ded07733d97ee235693525b29bf9285d6a
Eduardo Salinas <eduardoluis93@gmail.com>
2022-09-02 21:50:42 +0100

    Added Cookie Consent Banner

--- keith_app/static/js/script.js
+++ keith_app/static/js/script.js
@@ -1,2 +1,18 @@
 /* DEBUG message */
 console.log("The static folder is being detected.")
+
+/* JS to show the cookie consent banner, and hide it if the user accepts the web app's cookies (source:
+https://github.com/Godsont/Cookie-Consent-Banner/blob/master/main.js .) */
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
