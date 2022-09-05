7a846a453b21a5acd82d408b207c197052f1a29b
Eduardo Salinas <eduardoluis93@gmail.com>
2022-09-05 12:51:21 +0100

    Created 3 JS files for Combat Code

---
+++ keith_app/static/js/index.js
@@ -0,0 +1,8 @@
+/* I will use a huge chunk of JS code for the game from a specific tutorial in YouTube (source: Channel A (Alex Ziska)
+from https://youtu.be/vyqbNFMDRGQ ).
+
+I will need to create multiple JS files in order for the code to be readable. Remember to put the links to these JS
+files on the layout.html file so that I can load those scripts. I will use the same file structure as the one in the
+fighting game tutorial from YouTube from Channel A (Alex Ziska). That is, I will create the following 3 files: index.js,
+utils.js, and classes.js.
+*/
