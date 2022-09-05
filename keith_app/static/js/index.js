1e3dcb7595fa1e27aa752fb9779e6249aed1f166
Eduardo Salinas <eduardoluis93@gmail.com>
2022-09-05 17:11:17 +0100

    Removed Padding Below Navbar if User is Logged In

--- keith_app/static/js/index.js
+++ keith_app/static/js/index.js
@@ -9,10 +9,10 @@ utils.js, and classes.js.
 If I can make the size of the canvas responsive, that would be great, especially since I’m required to make this project to be responsive.
 If I set them to 1024 and 576, that's a fixed size in pixels. That's NOT what I want. I want a variable size.
 
-I will try Faheel’s answer from this Stack Overflow thread (source: Faheel’s reply from 
-https://stackoverflow.com/questions/34772957/how-to-make-canvas-responsive ). That is, I will set the canvas’ 
-height via JS. I will try setting the height to 0.5 times the width if the height of the screen is too low (like 
-less than 400px, which is the approximate height of mobile phones in horizontal mode). I will use a height of 
+I will try Faheel’s answer from this Stack Overflow thread (source: Faheel’s reply from
+https://stackoverflow.com/questions/34772957/how-to-make-canvas-responsive ). That is, I will set the canvas’
+height via JS. I will try setting the height to 0.5 times the width if the height of the screen is too low (like
+less than 400px, which is the approximate height of mobile phones in horizontal mode). I will use a height of
 less than 413 px for the conditional, since, for the hone screens I’m using, the phone height in horizontal mode is of 412 px.
 
 Since I always want the height of the canvas to be responsive, and, in general, I want the player to only play this game
@@ -27,7 +27,7 @@ const gameCanvas = document.querySelector('canvas')
 const c = gameCanvas.getContext('2d')
 
 // Ratio between the canvas' height and width
-const heightWidthRatio = 0.1
+const heightWidthRatio = 0.3
 
 // Height of the canvas (responsive)
 gameCanvas.height = gameCanvas.width * heightWidthRatio
