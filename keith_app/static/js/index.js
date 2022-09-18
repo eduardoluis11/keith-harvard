2f8173db47514dda2768f64f5d068698527453f4
Eduardo Salinas <eduardoluis93@gmail.com>
2022-09-18 22:46:40 +0100

    BUGFIX: Canvas is now Above Footer

--- keith_app/static/js/index.js
+++ keith_app/static/js/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable object-shorthand */
+/* eslint-disable no-var */
+/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-trailing-spaces */
 /* I will use a huge chunk of JS code for the game from a specific tutorial in YouTube (source: Channel A (Alex Ziska)
 from https://youtu.be/vyqbNFMDRGQ ).
@@ -29,19 +33,151 @@ adding or deleting the Bootstrap class that adds the padding depending on whethe
 the height to be higher. 
 
 What I wanted to do with an “if” won’t work. So, I will leave the ratio in 0.38 for all screens for the screen’s height.
+
+Next, I will create a class/constructor for creating sprites. From that class, I will create each instance of a sprite
+(like for the player, each anemy, and NPCs such as Keith). The "this" attribute refers to each specific instance of a 
+sprite.
+
+After that, I will create the player and the enemies by calling instances of the Sprite class, and specifying their
+position as arguments. Afterwards, I will render those sprites in the browser by calling the "draw()" function.
+
+Due to a bug, I will be forced to remove the ratio constant between the width and height of the canvas. I will have to 
+use another method for adapting the canvas to mobile screens.
+
+If I use the dimensions 1024x576 px, I will get a 16:9 ratio.
+
+AFTER FURTHER CONSIDERATION, I will use a JS game engine called Phaser. I will no longer use the code from Channel A (Alex 
+Ziska)'s Figthing Game. 
+
+First, I will add all of the basic stuff to start a Phaser game, which is a “config” variable, and 3 functions: create(), 
+update(), and preload() (source: https://phaser.io/tutorials/making-your-first-phaser-3-game/part1 .)
+
+Second, I will insert all of the sprites (or at least, some of the sprites for the time being) into Phaser’s preload() 
+function (source: https://phaser.io/tutorials/making-your-first-phaser-3-game/part2 .) I will insert the backgrounds as 
+images (with the “image” tag,) and the characters as spritesheets (with the “spritesheet” tag.)
+
 */
 
+// Settings for the Phaser game (includes physics, dimensions of the canvas, etc)
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'keith-game',
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+}
+
+// This executes the settings in "config" on the actual game
+var game = new Phaser.Game(config)
+
+// Here, I will insert the sprites
+function preload ()
+{
+  // Background of action level 1 without the ground
+  this.load.image('bg-level-1', 'assets/level-1/forest-background.jpg')
+
+  // Ground of action level 1
+  this.load.image('ground-level-1', 'assets/level-1/ground.jpg')
+
+  // Fang's idle spritesheet
+  this.load.spritesheet('fang-idle', 'assets/fang/fang-idle.png', { frameWidth: 32, frameHeight: 48 }
+  )
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // This selects the <canvas> tags
-const gameCanvas = document.querySelector('canvas')
+// const gameCanvas = document.querySelector('canvas')
 
-// This is the context variable, and will make the game 2D
-const c = gameCanvas.getContext('2d')
+// // This is the context variable, and will make the game 2D
+// const c = gameCanvas.getContext('2d')
 
-// Ratio between the canvas' height and width
-const heightWidthRatio = 0.38
+// // Canvas height
+// gameCanvas.width = 1024
 
-// Height of the canvas (responsive)
-gameCanvas.height = gameCanvas.width * heightWidthRatio
+// // Canvas width
+// gameCanvas.height = 576
+
+// // Ratio between the canvas' height and width
+// // const heightWidthRatio = 0.38
+
+// // Height of the canvas (responsive)
+// // gameCanvas.height = gameCanvas.width * heightWidthRatio
+
+// // This changes the color of the canvas
+// c.fillRect(0, 0, gameCanvas.width, gameCanvas.height)
+
+// // This is a class/method for creating sprites
+// class Sprite {
+//   // This stores each property of the class/method (like position)
+//   constructor (position) {
+//     this.position = position
+//   }
+
+//   // This will render an instance of a sprite
+//   draw () {
+//     // This is a placeholder that will render a red rectangle as a sprite
+//     c.fillStyle = 'red'
+
+//     // Dimensions of the rectangle that will act as a placeholder sprite
+//     c.fillRect(this.position.x, this.position.y, 50, 150)
+//   }
+// }
+
+// // Instance of the Sprite class, which will create the player
+// const player = new Sprite({
+//   x: 0,
+//   y: 0
+// })
+
+// // This renders the player
+// player.draw()
+
+// // Instance of an enemy sprite
+// const enemy = new Sprite({
+//   x: 400,
+//   y: 100
+// })
+
+// enemy.draw()
+
+// // DEBUG msg
+// console.log(player)
+
+
+
+
+
+
+
+
 
 // This checks if the the screen height is less than 413 px (a phone screen in horizontal mode)
 // if (gameCanvas.height < 413) {
