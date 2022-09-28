7673deb5e609a3f3914024482d8f089ee981a2b3
Eduardo Salinas <eduardoluis93@gmail.com>
2022-09-28 15:49:55 +0100

    I'm created the model for the Save Files

--- keith_app/models.py
+++ keith_app/models.py
@@ -13,3 +13,17 @@ I need first to create the model that stores users. I will use AbstractUser to c
 """
 class User(AbstractUser):
     pass
+
+""" Save File model.
+
+This will let me save the player's data when they save their game while talking to Keith. This will let me store the 
+player's current level, their HP, and their attack points. I will also need their user ID, so that I save the data
+from that specific user.
+
+All 4 fields are integers.
+"""
+class SaveFile(models.Model):
+    user_id = models.IntegerField(default=0)
+    player_level = models.IntegerField(default=0)
+    player_hp = models.IntegerField(default=0)
+    player_attack_points = models.IntegerField(default=0)
